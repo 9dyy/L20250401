5bb32d926a42bd1fd9fed523252b99e3de39ba82
9dyy <79816837+9dyy@users.noreply.github.com>
2025-04-01 09:15:51 +0900

    250401_사칙연산

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void main()
+{
+	int a = 3, b = 5;
+	float c = 4.0f, d = 7.0f;
+
+	cout << a + b << "\n";
+	cout << a - b << "\n";
+	cout << a * b << "\n";
+	cout << a / b << "\n";
+	cout << "\n";
+	cout << a + b << "\n";
+	cout << a - b << "\n";
+	cout << a * b << "\n";
+	cout << double(a) / b << "\n";
+}
